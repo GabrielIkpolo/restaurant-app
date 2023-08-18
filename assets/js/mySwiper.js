159aaffc4362a2b765580e9749f98044609dcf08
GabrielIkpolo <contact.gikps@gmail.com>
2023-08-18 16:34:39 +0100

    swiper implementation complete (desktop n mobile)

--- assets/js/mySwiper.js
+++ assets/js/mySwiper.js
@@ -1,21 +1,39 @@
+var swiper;
 
-// Fresh Start 
+const initSwiper = () => {
+  if (window.innerWidth <= 768) {
+    swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2, // Set to 2 for mobile devices
+      // centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } else {
+    swiper = new Swiper(".mySwiper", {
+      slidesPerView: 6, // Default value
+      // centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+}
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+// Initialize Swiper on page load
+initSwiper();
 
-
-// window.addEventListener('load', ()=>{
-
-// });
+// Update Swiper on window resize
+window.addEventListener("resize", initSwiper);
