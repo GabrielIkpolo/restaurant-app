ff178df6eeca70ea555fc92e20a76ea72fc99c0f
GabrielIkpolo <contact.gikps@gmail.com>
2023-09-20 14:14:35 +0100

    fixed swiper alignment

--- assets/js/mySwiper.js
+++ assets/js/mySwiper.js
@@ -37,3 +37,60 @@ initSwiper();
 
 // Update Swiper on window resize
 window.addEventListener("resize", initSwiper);
+
+
+
+
+// const initSwiper = () => {
+//   if (window.innerWidth <= 768) {
+//     swiper = new Swiper(".mySwiper", {
+//       slidesPerView: 2, // Set to 2 for mobile devices
+//       // centeredSlides: true,
+//       spaceBetween: 30,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
+//   } else {
+//     swiper = new Swiper(".mySwiper", {
+//       slidesPerView: 6, // Default value
+//       // centeredSlides: true,
+//       spaceBetween: 30,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
+//   }
+
+//   // Add event listeners to the navigation buttons
+//   const nextButton = document.querySelector(".swiper-button-next");
+//   const prevButton = document.querySelector(".swiper-button-prev");
+
+//   nextButton.addEventListener("click", () => {
+//     if (!swiper.isEnd) {
+//       swiper.slideNext();
+//     }
+//   });
+
+//   prevButton.addEventListener("click", () => {
+//     if (!swiper.isBeginning) {
+//       swiper.slidePrev();
+//     }
+//   });
+// };
+
+// // Initialize Swiper on page load
+// initSwiper();
+
+// // Update Swiper on window resize
+// window.addEventListener("resize", initSwiper);
